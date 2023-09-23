34b1c240e1e8e3180557ffd8f16db5c829126c9e
komashige <koma.shige0128@gmail.com>
2023-09-23 10:49:38 +0900

    バリデーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  class User < ApplicationRecord
+    validates :email, presence: { message: "" }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, presence: { message: "" }, length: { minimum: 6 }
+    validates :password_confirmation, presence: { message: "" }
+    validates :name, presence: { message: "" }
+  end# 他の属性に対するバリデーションを追加することもできます
 end
