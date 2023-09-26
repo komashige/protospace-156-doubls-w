726475d5b13e754803ea6f4c28fdd3d468202a29
komashige <koma.shige0128@gmail.com>
2023-09-26 15:30:10 +0900

    ユーザー詳細完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'protospaces#index'
-  resources :protospaces, only: [:index, :new, :create, :destroy, :show, :edit, :update,]
-  resources :users, only: [:edit, :update, :new, :create, :show ]
+  resources :protospaces
+  resources :users, only: :show
 end
